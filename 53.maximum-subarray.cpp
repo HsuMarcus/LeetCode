4aab95c9d22a7555b99c0ab55f4819c8db0fc4fd
MarcusHsu <z5167306@ad.unsw.edu.au>
2021-11-22 23:40:03 +0800

    Added 53

--- 53.maximum-subarray.cpp
+++ 53.maximum-subarray.cpp
@@ -11,12 +11,14 @@ public:
         int max = nums[0];
         int temp = 0;
         for(int i = 0; i < nums.size(); i++){
-            for(int j = i; j < nums.size(); j++){
-                temp = 0;
-                for(int k = i; k <= j ;k++){
-                    temp += nums[k];   
-                }
-                if(temp > max) max = temp; 
+            if((temp + nums[i]) > nums[i]) {
+                temp += nums[i];
+            }else{
+                temp = nums[i];
+            }
+
+            if(temp > max){
+                max = temp;
             }
         }
         return max; 
